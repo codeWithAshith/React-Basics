be2937d6825429bc862b99663c0728320d94826a
codewithashith <codewithashith@gmail.com>
2023-02-04 19:54:03 +0530

    13. Advance React Setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h2>Advance React</h2>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import BookList from "./BookList";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BookList />
+    <App />
   </React.StrictMode>
 );
