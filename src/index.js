bbae424a4c2e877164355ce2b1b03f257ce0150c
codewithashith <codewithashith@gmail.com>
2023-02-04 19:02:06 +0530

    10. Mini Project - key and props

--- src/index.js
+++ src/index.js
@@ -6,18 +6,21 @@ import "./index.css";
 
 const books = [
   {
+    id: 1,
     title: "The Psychology of Money Paperback",
     author: "Morgan Housel",
     img: "https://m.media-amazon.com/images/I/41r6F2LRf8L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
     price: 10,
   },
   {
+    id: 2,
     title: "Ikigai",
     author: "Héctor García",
     img: "https://m.media-amazon.com/images/P/178633089X.01._SCLZZZZZZZ_SX500_.jpg",
     price: 10,
   },
   {
+    id: 3,
     title: "8 Rules of Love",
     author: "Jay Shetty",
     img: "https://m.media-amazon.com/images/I/712V6e98ZsL.jpg",
@@ -28,33 +31,20 @@ const books = [
 const BookList = () => {
   return (
     <section className="booklist">
-      {books.map((book, index) => (
-        <Book
-          title={book.title}
-          author={book.author}
-          img={book.img}
-          price={book.price}
-        >
-          Description about the book
-        </Book>
+      {books.map((book) => (
+        <Book key={book.id} {...book} />
       ))}
     </section>
   );
 };
 
-const Book = ({ title, author, img, price, children }) => {
-  // const { title, author, img, price } = props;
+const Book = ({ title, author, img, price }) => {
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
-      <h4
-        style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
-      >
-        {author.toUpperCase()}
-      </h4>
-      <p>Price $ {price}</p>
-      {children}
+      <h4>{author.toUpperCase()}</h4>
+      <p>Price $ {price}</p>s{" "}
     </article>
   );
 };
