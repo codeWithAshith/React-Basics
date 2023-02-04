2a5fbcafa30ad4af3f73ae974960d1f289ba3d28
codewithashith <codewithashith@gmail.com>
2023-02-04 18:21:52 +0530

    07. Mini Project - props

--- src/index.js
+++ src/index.js
@@ -4,32 +4,32 @@ import ReactDOM from "react-dom/client";
 //css
 import "./index.css";
 
+const author = "Morgan Housel";
+const title = "The Psychology of Money Paperback";
+const img =
+  "https://m.media-amazon.com/images/I/41r6F2LRf8L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg";
+const price = 10;
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      <Book title={title} author={author} img={img} price={price} />
     </section>
   );
 };
 
-const author = "Morgan Housel";
-const Book = () => {
-  const title = "The Psychology of Money Paperback";
+const Book = (props) => {
+  const { title, author, img, price } = props;
   return (
     <article className="book">
-      <img
-        src="https://m.media-amazon.com/images/I/41r6F2LRf8L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"
-        alt=""
-      />
+      <img src={img} alt="" />
       <h1>{title}</h1>
       <h4
         style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
       >
         {author.toUpperCase()}
       </h4>
-      <p>Price $ {6 + 9}</p>
+      <p>Price $ {price}</p>
     </article>
   );
 };
