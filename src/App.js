3bb21c11e9863a084d862666439540bef7d80abe
codewithashith <codewithashith@gmail.com>
2023-02-04 20:00:33 +0530

    14. UseState - basics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "./index.css";
 
 const App = () => {
+  const [text, setText] = useState("Random Title");
+  const handleClick = () => {
+    if (text === "Random Title") {
+      setText("Hello World");
+    } else {
+      setText("Random Title");
+    }
+  };
+
   return (
     <div className="container">
-      <h2>Advance React</h2>
+      <h2>{text}</h2>
+      <button className="btn" onClick={handleClick}>
+        Change Title
+      </button>
     </div>
   );
 };
